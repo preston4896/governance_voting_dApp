ac6af4f3a8a842b34e7999a84b55780a728dfb86
preston4896 <preston4896@me.com>
2021-03-12 14:10:50 +0800

    Added vote function and passed test.

--- src/contracts/Vote.sol
+++ src/contracts/Vote.sol
@@ -8,7 +8,6 @@ contract Vote {
     
     using SafeMath for uint256;
     address public admin; // contract admin
-    uint256 public balance; // contract eth balance
 
     constructor() public {
         admin = msg.sender;
@@ -19,10 +18,11 @@ contract Vote {
      * @param id - Unique identifier for the proposal.
      * @param proposer - The address created the proposal.
      * @param title - The description of the proposal.
-     * @param yay_count - Count votes for the proposal.
-     * @param nay_count - Count votes against the proposal.
+     * @param yay_count - Count votes in proportion to their deposit eth amount for the proposal.
+     * @param nay_count - Count votes in proportion to their deposit eth amount against the proposal.
      * @param deposit_balance - The total amount of ETH deposited for the proposal.
-     * @param proposer_stake - The maximum amount of ETH set by the proposer that can be deposited by all voters, to prevent whales from manipulating vote results.
+     * @param begin_block_number - The block number when the proposal is created.
+     * @param end_block_number - The block number when the proposal becomes inactive.
      */
     struct Proposal {
         uint256 id;
@@ -31,7 +31,6 @@ contract Vote {
         uint256 yay_count;
         uint256 nay_count;
         uint256 deposit_balance;
-        uint256 proposer_stake;
         uint256 begin_block_number;
         uint256 end_block_number;
     }
@@ -44,37 +43,73 @@ contract Vote {
 
     uint256 public total_proposals;
     mapping (uint256 => Proposal) public Proposals; // Find the proposals with the given ID.
-    mapping (address => mapping (uint256 => Voter_Status)) private addressToVote; // Show votes given by address and id.
+    mapping (uint256 => mapping (address => Voter_Status)) private addressToVote; // Show votes given by address and id.
     mapping (uint256 => mapping (uint => address[])) private voteToAddress; // Show the addresses correspond to a vote, requires id input.
 
     // Keeping track of active proposals.
     mapping (uint256 => uint256[]) private active_proposals; // block end number mapped to array of proposal ids.
 
+    // Keeping track of user deposited amount
+    mapping (uint256 => mapping(address => uint256)) private deposit;
+
     event Transfer(address indexed _from, address indexed _to, uint256 amount); // Transfer of ETH event.
+    event Voted(address indexed _voter, uint256 id, bool votesYay); // Users cast votes event.
 
     /**
      * @dev Function to create a proposal, requires a minimum deposit amount of 0.001 ETH.
      * @return The proposal id
      */
-    function create(address payable _proposer, string memory title, uint endOffset) public payable returns(uint256) {
-        require(msg.value >= 0.001 ether, "Deposit does not meet the minimum requirement.");
-        require(endOffset > 0, "End block number undefined.");
+    function create(string memory title, uint endOffset) public payable returns(uint256) {
+        require(msg.value >= 0.001 ether, "Deposit does not meet the minimum requirement");
+        require(endOffset > 0, "End block number undefined");
 
         uint id = total_proposals.add(1);
         total_proposals = id;
         uint endBlock = block.number.add(endOffset);
-        Proposal memory newProposal = Proposal(id, _proposer, title, 1, 0, msg.value, msg.value, block.number, endBlock);
+        Proposal memory newProposal = Proposal(id, msg.sender, title, msg.value, 0, msg.value, block.number, endBlock);
         Proposals[total_proposals] = newProposal;
         active_proposals[newProposal.end_block_number].push(id);
 
+        deposit[id][msg.sender] = msg.value;
+
         // Proposer votes yay by default.
-        addressToVote[_proposer][id] = Voter_Status.YAY;
-        voteToAddress[id][uint(Voter_Status.YAY)].push(_proposer);
+        addressToVote[id][msg.sender] = Voter_Status.YAY;
+        voteToAddress[id][uint(Voter_Status.YAY)].push(msg.sender);
 
-        balance = balance.add(msg.value);
-
-        emit Transfer(_proposer, address(this), msg.value);
+        emit Transfer(msg.sender, address(this), msg.value);
 
         return id;
     }
+
+    /**
+     * @dev Function to vote on a proposal.
+     */
+    function vote(uint256 id, bool votesYay) public payable returns(bool success) {
+        require(id <= total_proposals, "Invalid proposal");
+        require(addressToVote[id][msg.sender] == Voter_Status.UNDECIDED, "Can not vote twice");
+        Proposal storage proposal = Proposals[id];
+        require(proposal.end_block_number > block.number, "Proposal is no longer active");
+        require(msg.value >= 0.001 ether, "Deposit does not meet the minimum requirement");
+        uint maximum = deposit[id][proposal.proposer].mul(90).div(100); // voters can only deposit 90% of the proposer's amount at most -- prevention of whales.
+        require(msg.value <= maximum, "Deposit exceeded the maximum amount");
+
+        proposal.deposit_balance = proposal.deposit_balance.add(msg.value);
+        deposit[id][msg.sender] = msg.value;
+
+        if (votesYay) {
+            addressToVote[id][msg.sender] = Voter_Status.YAY;
+            voteToAddress[id][uint(Voter_Status.YAY)].push(msg.sender);
+            proposal.yay_count = proposal.yay_count.add(msg.value);
+        }
+        else {
+            addressToVote[id][msg.sender] = Voter_Status.NAY;
+            voteToAddress[id][uint(Voter_Status.NAY)].push(msg.sender);
+            proposal.nay_count = proposal.nay_count.add(msg.value);
+        }
+
+        emit Voted(msg.sender, id, votesYay);
+
+        return true;
+    }
+
 }
--- test/test_vote.test.js
+++ test/test_vote.test.js
@@ -11,40 +11,88 @@ contract("Vote", (accounts) => {
 
     it("1. Create Proposals", async() => {
         let title = "Hello, World!";
-        let offset = 10; // 10 blocks
+        let offset = 20; // 20 blocks
         let deposit_amount = web3.utils.toWei("0.08");
 
         // attempts to stake more eth than balance.
         try {
-            await vote.create(accounts[0], title, offset, {value: web3.utils.toWei("200")});
+            await vote.create(title, offset, {value: web3.utils.toWei("200")});
         } catch (error) {
             assert(error.message.indexOf("sender") >= 0, "sender error message.");
         }
 
         // // attempts to stake lower than the minimum requirement.
         try {
-            await vote.create(accounts[0], title, offset, {value: web3.utils.toWei("0.00002")});
+            await vote.create(title, offset, {value: web3.utils.toWei("0.00002")});
         } catch (error) {
             assert(error.message.indexOf("revert") >= 0, "error message must contain revert.");
         }
 
         // invalid endblockTime
         try {
-            await vote.create(accounts[0], title, 0, {value: deposit_amount});
+            await vote.create(title, 0, {value: deposit_amount});
         } catch (error) {
             assert(error.message.indexOf("revert") >= 0, "error message must contain revert.");
         }
 
         // proposal successfully created.
-        await vote.create(accounts[0], title, offset, {value: deposit_amount});
+        await vote.create(title, offset, {value: deposit_amount});
 
         // there should be one proposal.
         let total_proposals = await vote.total_proposals();
         assert.equal(total_proposals, 1, "1 proposal only.");
 
-        // check contract balance.
-        let expected_bal = await vote.balance();
-        let actual_bal = await web3.eth.getBalance(vote.address);
-        assert.equal(actual_bal, expected_bal, "balance should match.");
+        // creates another proposal
+        await vote.create(title, 2, {from: accounts[1], value: deposit_amount});
+
+        // there should be two proposals.
+        total_proposals = await vote.total_proposals();
+        assert.equal(total_proposals, 2, "2 proposals only.");
+    })
+
+    it("2. Cast Votes", async() => {
+        let vote_amount = web3.utils.toWei("0.02");
+
+        // voter attempts to vote on non-existent proposal
+        try {
+            await vote.vote(3, false, {value: vote_amount});
+        } catch (error) {
+            assert(error.message.indexOf("revert") >= 0, "error message must contain revert.");
+        }
+
+        // proposer attempts to vote (multiple votes)
+        try {
+            await vote.vote(1, false, {value: vote_amount});
+        } catch (error) {
+            assert(error.message.indexOf("revert") >= 0, "error message must contain revert.");
+        }
+
+        // attempts to vote on expired proposal
+        try {
+            await vote.vote(2, false, {value: vote_amount});
+        } catch (error) {
+            assert(error.message.indexOf("revert") >= 0, "error message must contain revert.");
+        }
+
+        // attempts to deposit more eth than proposer.
+        try {
+            await vote.vote(1, false, {from: accounts[1], value: web3.utils.toWei("1")});
+        } catch (error) {
+            assert(error.message.indexOf("revert") >= 0, "error message must contain revert.");
+        }
+
+        // accounts 1 voted yay, while accounts 2 voted nay.
+        await vote.vote(1, true, {from: accounts[1], value: vote_amount});
+        await vote.vote(1, false, {from: accounts[2], value: vote_amount});
+
+        // verify deposited eth balance.
+        let prop_1 = await vote.Proposals(1);
+        let yay = prop_1.yay_count;
+        let nay = prop_1.nay_count;
+        let total = prop_1.deposit_balance;
+
+        assert.equal(total, web3.utils.toWei("0.12"));
+        assert.equal(yay, web3.utils.toWei("0.1"));
+        assert.equal(nay, vote_amount);
     })
 })
