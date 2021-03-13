190dd55f508ca314a6544ffb564718db7ff8acde
preston4896 <preston4896@me.com>
2021-03-14 01:06:46 +0800

    Contract loaded successfully.

--- src/js/jsx/app.jsx
+++ src/js/jsx/app.jsx
@@ -2,6 +2,7 @@
 
 class App extends React.Component {
 
+    // checks for web3-compatible wallet
     async loadWeb3() {
         // web3 browser
         if (window.ethereum) {
@@ -13,8 +14,36 @@ class App extends React.Component {
             window.web3 = new Web3(window.web3.currentProvider);
         }
         else {
-            window.alert("Browser does not support Web3. Consider installing MetaMask.");
+            let message = "Browser does not support Web3. Consider installing MetaMask.";
+            let alert = confirm(message);
+            if (alert) {
+                window.open("https://metamask.io/", "_blank", "noopener, noreferrer");
+            }
         }
+    }
+
+    // load user's account and contract
+    async loadData() {
+        const web3 = window.web3;
+        const accounts = await web3.eth.getAccounts();
+
+        this.setState({account: accounts[0]});
+
+        const networkId = await web3.eth.net.getId();
+
+        // load the contract
+        let abi;
+        let address;
+        await fetch("./src/builds/Vote.json")
+         .then(body => body.json())
+         .then(data => {
+            abi = data.abi;
+            address = data.networks[networkId].address;
+            const vote = new web3.eth.Contract(abi, address);
+            this.setState({voteContract: vote});
+         }).catch((error) => {
+            window.alert("The contract is not deployed to this network.");
+         })
     }
 
     constructor(props) {
@@ -32,6 +61,7 @@ class App extends React.Component {
 
     async componentDidMount() {
         await this.loadWeb3();
+        await this.loadData();
     }
 }
 
